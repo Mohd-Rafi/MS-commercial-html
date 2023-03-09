36db16bb895bee262a2a032b555a9cccde08c5b1
Mohd-Rafi <mohd.rafi@adglobal360.com>
2023-03-09 15:17:00 +0530

    home and supercarry page

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+const plugin = require('tailwindcss/plugin');
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: ["./**/*.{html,js}"],
+//   content: [
+//     "./index.html",
+// ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#DA251D',
+        'secondary': '#333333'
+      },
+      fontSize: {
+        '13': '0.8125rem',
+        '22': '1.375rem',
+        '21': '1.3125rem',
+        '15': '0.9375rem',
+      },
+    },
+    fontFamily:{
+      'body': ['Roboto', 'sans-serif'],
+      'montserrat':['Montserrat', 'sans-serif'],
+      'gm':['gorditamedium'],
+      'gb':['gorditabold'],
+    },
+    backgroundImage: {
+      'footer-nav-arrow': "url('../images/footer-mob-arrow.webp')",
+    },
+    screens: {
+      'sm': {'max': '360px'},
+      ...defaultTheme.screens,
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
+
+
